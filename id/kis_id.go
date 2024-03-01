8be17a21c65f12578e7af2d69dfb41455f0fa151
dufei <wd345901051@163.com>
2024-03-01 18:09:05 +0800

    stop

---
+++ id/kis_id.go
@@ -0,0 +1,12 @@
+package id
+
+// func KisID(prefix ...string) (kisId string) {
+// 	idStr := strings.Replace(uuid.New().String(), "-", "", -1)
+// }
+
+// func formatKisID(idStr string,prefix ...string)string{
+// 	var kisId string
+// 	for _,fix:=range prefix{
+// 		kisId +=fix
+// 	}
+// }
